990ef83343c3be3b1710333a4150bc8e048bb95e
Dallin Osmun <dosmun@gmail.com>
2022-05-24 16:26:08 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Make Awesome
+
+```sh
+yarn create remix --template numso/rmx
+```
---
+++ app/routes/index.jsx
@@ -0,0 +1,3 @@
+export default function Index () {
+  return <div className='text-2xl text-center mt-8'>Make Awesome</div>
+}
